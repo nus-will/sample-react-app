697f0307d8ba9ec97e9173e1eebff31dd2a295c4
Will <will@nustechnology.com>
2022-01-13 23:04:49 +0700

    Add require login authentication

--- src/components/RoutingSystem/index.js
+++ src/components/RoutingSystem/index.js
@@ -1,18 +1,28 @@
 import {
   Routes,
-  Route
+  Route,
+  Navigate
 } from "react-router-dom";
-
 import Home from '../Home'
 import Login from '../Login'
 import Detail from '../Detail'
+import { useSelector } from 'react-redux'
+
+const requireLogin = (component, loggedIn) => {
+  const LoggedInComponent = component
+  return loggedIn ?
+    <LoggedInComponent /> :
+    <Navigate to="/login" />
+}
+
 
 function RoutingSystem() {
+  let loggedIn = useSelector(state => state.user.loggedIn)
   return (
     <Routes>
       <Route path="/login" element={<Login />} />
-      <Route path="/detail" element={<Detail />} />
-      <Route path="/" element={<Home />} />
+      <Route path="/detail/:id" element={requireLogin(Detail, loggedIn)} />
+      <Route path="/" element={requireLogin(Home, loggedIn)} />
     </Routes>
   );
 }
