d0633894e7f14e77d98de9d97f9e9714f663922e
Will <will@nustechnology.com>
2022-01-14 09:33:46 +0700

    Add delete and view detail action

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,37 +1,51 @@
 import { useSelector } from 'react-redux'
 import { Table, Space } from 'antd'
 import './home.css'
-
-const columns = [
-  {
-    title: 'Name',
-    dataIndex: 'name',
-    key: 'name',
-  },
-  {
-    title: 'Created By',
-    dataIndex: 'createdBy',
-    key: 'createdBy',
-  },
-  {
-    title: 'Created Date',
-    dataIndex: 'createdDate',
-    key: 'createdDate',
-  },
-  {
-    title: 'Action',
-    key: 'action',
-    render: (text, record) => (
-      <Space size="middle">
-        <a>View details</a>
-        <a>Delete</a>
-      </Space>
-    ),
-  },
-];
+import { deleteProject } from '../../redux/reducers/projects'
+import { useDispatch } from 'react-redux'
+import { useNavigate } from "react-router-dom";
 
 function Home() {
+
   let projects = useSelector(state => state.projects)
+  let navigate = useNavigate();
+  const dispatch = useDispatch()
+
+  const onDelete = (projectId) => {
+    dispatch(deleteProject(projectId))
+  }
+
+  const onViewDetails = (projectId) => {
+    navigate("/detail/" + projectId);
+  }
+
+  const columns = [
+    {
+      title: 'Name',
+      dataIndex: 'name',
+      key: 'name',
+    },
+    {
+      title: 'Created By',
+      dataIndex: 'createdBy',
+      key: 'createdBy',
+    },
+    {
+      title: 'Created Date',
+      dataIndex: 'createdDate',
+      key: 'createdDate',
+    },
+    {
+      title: 'Action',
+      key: 'action',
+      render: (text, record) => (
+        <Space size="middle">
+          <a onClick={() => onViewDetails(record.id)} >View detail</a>
+          <a onClick={() => onDelete(record.id)} >Delete</a>
+        </Space>
+      ),
+    },
+  ];
 
   return (
     <div id="home-projects">
