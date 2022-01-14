792e31b9c449b0e1d3cf7a39ef02ea752f999b5a
Will <will@nustechnology.com>
2022-01-14 11:35:51 +0700

    Add avatar icon after logged in

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,18 +1,29 @@
 import { Link } from "react-router-dom";
 import {
   Layout,
-  Menu
+  Menu,
+  Avatar
 } from 'antd';
+import { UserOutlined } from '@ant-design/icons';
+import { useSelector } from 'react-redux'
 import './header.css'
 
 const { Header } = Layout;
 
 function SystemHeader() {
+  let {loggedIn, username} = useSelector(state => state.user)
   return (
     <div id="header">
       <Header>
+        {
+          loggedIn && <span className='profile'>
+            {username}
+            <Avatar icon={<UserOutlined />} />
+          </span>
+        }
+
         <Menu theme="dark" mode="horizontal">
-          <Menu.Item >
+          <Menu.Item key='home'>
             <Link to="/">Home</Link>
           </Menu.Item>
         </Menu>
